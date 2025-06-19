e3361b1792eb241c13a7f25b2af743b8d672cca9
Rahul77 <149512376+ukkadapu-rahul@users.noreply.github.com>
2025-06-19 13:01:04 +0530

    Update preload.js

--- RestIris project/src/preload.js
+++ RestIris project/src/preload.js
@@ -4,6 +4,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
   minimizeWindow: () => ipcRenderer.send('minimize-window'),
   closeWindow: () => ipcRenderer.send('close-window'),
   restoreWindow: () => ipcRenderer.send('restore-window'),
-  showNotification: (title, body) => ipcRenderer.send('show-notification', title, body),
-  startDrag: () => ipcRenderer.send('start-drag') 
+  showNotification: (title, body) => ipcRenderer.send('show-notification', title, body)
 });
